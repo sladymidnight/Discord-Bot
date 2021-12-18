eec0780f5cf4b362cfe7236d81701d9b2d73874a
sladymidnight <86133270+sladymidnight@users.noreply.github.com>
2021-12-18 09:49:32 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+import os 
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv():
+
+client = Discord.Client()
+
+@client.event
+async def on_ready():
+  print("Bot is up and ready!")
+  
+@client.event
+async def hello( ):
+  await ctx.send("Hi!")
+  
+client.run(" OTIxNTIyMDU2NjQwNzQxNDQ3.Yb0ISw.rgYWY-PdoGWOSlJJLP2XIlMUJJM ")
