871ba07806d941057367eb4baf18a6e275ed9be2
sladymidnight <86133270+sladymidnight@users.noreply.github.com>
2021-12-18 13:37:30 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,5 +9,31 @@ client = discord.Client()
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
   
+@client.event
+async def on_message(message):
+  username = str(message.author).split('#')[0]
+  user_message = str(message.content)
+  channel = str(message.channel.name)
+  print(f'{username}: {user_message} ({channel})')
+  
+  if message.author = client.user
+    return
+  
+  if message.channel.name == 'general'
+    if user_message.lower() == 'hello':
+      await message.channel.send(f'Hello {username}!')
+      return
+    elif username.message() == 'bye':
+      await message.channel.send(f'Goodbye {username}!')
+      return
+    elif user_message.lower() == '!random'
+      response = f'This is your random number: {random.randrange(10000000)}
+      await message.channel.send(response)
+      return
+  
+  if user_message.lower() == '!anywhere':
+    await message.channel.send('This can be used anywhere!')
+    return
+  
 client.run(TOKEN)
 
