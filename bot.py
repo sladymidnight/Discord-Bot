d8505ecbb0c635723226b965a3bd74bf93a2dd5f
sladymidnight <86133270+sladymidnight@users.noreply.github.com>
2021-12-19 11:13:18 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,15 @@
-#you can find this code on Code Palace's youtube page
+# this function will add two values
+def add(x, y): 
+  return x + y
+# this fuction will subtract two values
+def subtract(x, y):
+  return x - y
+# this function will multiply two values
+def multiply(x, y):
+  return x * y
+# this function will divide two values
+def divide(x, y):
+  return x / y
 
 import discord
 import random
@@ -21,7 +32,7 @@ async def on_message(message):
   if message.author == client.user:
     return
   
-  if message.channel.name == 'socialize':
+  if message.channel.name == 'bot-testing':
     if user_message.lower() == 'hello': 
       await message.channel.send(f'Hello {username}!')
       return
@@ -32,10 +43,17 @@ async def on_message(message):
       response = f'This is your random number: {random.randrange(10000000)}'
       await message.channel.send(response)
       return
-  
-  if user_message.lower() == '!anywhere':
-    await message.channel.send('This can be used anywhere!')
-    return
+    elif user_message.lower() == '!ping':
+      await message.channel.send(f'Pong you dirty whore {username}!')
+      return
+    elif user_message.lower() == '!calculate':
+      response = f'Please select !add, !subtract, !multiply, !divide'
+      await message.channel.send(response)
+      return
+    
+    if user_message.lower() == '!anywhere':
+      await message.channel.send('This can be used anywhere!')
+      return
   
 client.run(TOKEN)
 
